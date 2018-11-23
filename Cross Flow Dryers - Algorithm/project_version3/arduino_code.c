e4281d90f5b9bcbe2ff37ea779d8689f0df7e185
higor21 <felype.cet.15@outlook.com>
2018-11-23 17:23:46 -0300

    update

--- Cross Flow Dryers - Algorithm/project_version3/arduino_code.c
+++ Cross Flow Dryers - Algorithm/project_version3/arduino_code.c
@@ -3,69 +3,85 @@ void setup(){
   //MISO - PB4
   //MOSI - PB3
   //SS - PB2
-  //clkArduino- PB1
-  //selData - PB0
-  //chave PD7
+  //chave - PB1
+  //Sensor Luminosidade - A0
+  //Sensor temperatura - A1
   
-  //Timer para contagem do tempo
-  TCCR1A = 0b10000000;
-  //Prescale de 256: 16MHz/64 = 250000
-  TCCR1B = 0b00011011;
-  //SE 250000 - 1 s
-  //   62500 - 1/4 s
-  ICR1 = (uint16_t) 62500;
-  //Mantem em pulso em 10 contagens para que registrador da FPGA
-  //contablize clock
-  OCR1A = (uint16_t) 10;
-	
-  //Ativa mestre SPI
-	SPCR = 0b01010001;
-	//SCK, MOSI e SS como saída e MISO como entrada e OCR1 como saída
-	DDRB = 0b00101111;
-	
-  //Configurando ADC
-	ADMUX = 0b01100000;
-	ADCSRA = 0b10000111;
+  //Define SCK (PB5), MOSI(PB3), SS(PB2) e chave(PB1) como saída e MISO(PB4) como entrada 
+  DDRB = 0b00101100;  
+  PORTB = 0b00000100;
   
-  //UART - baud 9600 
+  //SPI - Configuração
+  //Ativa o SPI
+  //Define como mestre SPI
+  //Seleciona o prescale de 128, de modo que a frequência do SPI fica (16M/128 = 125KHz)
+  SPCR = 0b01010011;
+  
+  //ADC - Configuração
+  //Ativa o ADC
+  //Define o prescale de 128, de forma que a frequência de entrada do ADC fique em (16M/128 = 125KHz)
+  //Usando Vref como referência de voltagem
+  //Ativa ADLAR para facilitar a obtenção dos 8 bits mais significativos
+  ADMUX = 0b01100000;
+  ADCSRA = 0b10000111;
+  
+  //USART - Configuração
+  //Modo assíncrono
+  //Baudrate de 9600 bits/s
+  //Ativa modo transmissão do USART
+  //Bit de paridade par
+  //8 bits por transmissão
+  //1 bit de parada
+  UCSR0A = 0b00000000;
   UCSR0B = 0b00001000;
   UCSR0C = 0b00100110;  
   UBRR0 = (uint8_t) 103;
 }
 
-uint8_t temp = 10;
-uint8_t lum = 10;
+//Respectivamente, variáveis que armazenam a temperatura  e luminosidade obtidas do ADC e o valor PWM recebido da FPGA
+uint8_t temp = 0;
+uint8_t lum = 0;
 uint8_t envia_pc = 0;
 
 void loop(){
-  //Aguarda chave estar em ON, esperando caso contrário
-  while(!(PIND & 0b10000000));
   
-	//ADC de 8 bits
-	ADMUX &= 0b11110000;
-	ADCSRA |= 0b01000000;
-	while(!(ADCSRA & 0b00010000));
-	temp = ADCH;	
+  //Aguarda chave estar ligada para iniciar o processo de envio e recebimento de dados
+  while(!(PINB & 0b00000010));
   
-  //ADC de 8 bits
-	ADMUX &= 0b11110001;
-	ADCSRA |= 0b01000000;
-	while(!(ADCSRA & 0b00010000));
-	temp = ADCH;	
+  //Obtem luminosidade por meio do ADC
+  ADMUX &= 0b11110000;
+  ADCSRA |= 0b01000000;
+  while(!(ADCSRA & 0b00010000));
+  lum = (uint8_t) ADCH;  
   
-  //envia e recebe dados da fpga
-  PORTB = 0b00000000;
-	SPDR = (uint8_t) temp;	
-	while(!SPSR & 0b10000000);
-	envia_pc = (uint8_t) SPDR;
+  //Obtem temperatura por meio do ADC
+  ADMUX |= 0b00000001;
+  ADCSRA |= 0b01000000;
+  while(!(ADCSRA & 0b00010000));
+  temp = (uint8_t) ADCH; 
   
-  //envia e recebe dados da fpga
-  PORTB = 0b00000001;
-	SPDR = (uint8_t) umid;	
-	while(!SPSR & 0b10000000);
-	envia_pc = (uint8_t) SPDR;
+  //Desativa porta SS para ativar transmissão de dados no escravo
+  PORTB &= 0b11111011;  
+  delayMicroseconds(100);
+
+  //Envia valor de luinosidade para a FPGA usando SPI
+  SPDR = (uint8_t) lum;  
+  while(!(SPSR & 0b10000000));
   
-  //Envia para o computador
+  //Liga e desliga porta SS para zerar registradores do escravos e recomeçar transmissão de dados
+  PORTB != 0b00000100;
+  delayMicroseconds(100);
+  PORTB &= 0b11111011;
+  delayMicroseconds(100);
+  
+  //Envia valor de temperatura para FPGA usando SPI, que retorna o valor do PWM
+  SPDR = (uint8_t) temp; 
+  while(!(SPSR & 0b10000000));
+  envia_pc = (uint8_t) SPDR;
+
+  //Envia dado para o computador usando USART
   while (!( UCSR0A & 0b00100000));
-	UDR0 = envia_pc;
+  UDR0 = envia_pc;
+  PORTB != 0b00000100;
+  delay(250);
 }
